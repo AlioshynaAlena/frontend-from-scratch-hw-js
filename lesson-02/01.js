f8064684027455145ab831af70b6f9605d0871c5
Alena Alioshyna <elenakovalski1995@yandex.ru>
2025-05-28 12:43:12 +0300

    two commit

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -8,11 +8,13 @@
 */
 
 // тестовые данные (значения можно менять)
+
+//your code
+
 const isAdmin = false
 const isVerifiedUser = true
 const hasSpecialPermission = true
 const hasTemporaryPass = false
 
-let isAccess
-
-// your code
+let isAccess = ((!isAdmin && isVerifiedUser) && (hasSpecialPermission || !hasTemporaryPass))
+console.log(isAccess);
