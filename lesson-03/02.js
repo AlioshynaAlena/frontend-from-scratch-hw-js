bdf3fbd6c08d3275b4a859c40b79fa32245c1d38
Alena Alioshyna <elenakovalski1995@yandex.ru>
2025-06-04 12:26:52 +0300

    seven commit

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,52 +1,10 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
-// const number = 0
-// let result;
-
-// function isEven(number) {
-//     if (number % 2 === 0) {
-//         result = true
-//     } else {
-//         result = false
-//     }
-// }
-// isEven(number)
-// console.log(result);
-
-
-// let number = 7
-// let value = 2
-// let result;
-
-// function isEven(number, value) {
-//     if (number % value === 0) {
-//         result = true
-//     } else {
-//         result = false
-//     }
-// }
-// isEven(number, value)
-// console.log(result);
-
-
-
-// можно использовать еще стрелочную функцию, тернарный оператор
-
-
 const number = 0
 
 function isEven(number) {
-    return number % 2 === 0 ? "True" : "False"
-
+    return number % 2 === 0;
 }
 const result = isEven(number)
 console.log(result);
-
-// const number = 0
-
-// function isEven(number) {
-//     return number % 2 === 0;
-// }
-// const result = isEven(number)
-// console.log(result);
