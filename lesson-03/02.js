56aef7a690b2d16812834a9d43d61152ed1130c6
Alena Alioshyna <elenakovalski1995@yandex.ru>
2025-06-04 12:20:46 +0300

    five commit

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -15,20 +15,38 @@
 // console.log(result);
 
 
-let number = 7
-let value = 2
-let result;
+// let number = 7
+// let value = 2
+// let result;
 
-function isEven(number, value) {
-    if (number % value === 0) {
-        result = true
-    } else {
-        result = false
-    }
-}
-isEven(number, value)
-console.log(result);
+// function isEven(number, value) {
+//     if (number % value === 0) {
+//         result = true
+//     } else {
+//         result = false
+//     }
+// }
+// isEven(number, value)
+// console.log(result);
 
 
 
 // можно использовать еще стрелочную функцию, тернарный оператор
+
+
+// const number = 0
+
+// function isEven(number) {
+//     return number % 2 === 0 ? "True" : "False"
+
+// }
+// const result = isEven(number)
+// console.log(result);
+
+const number = 0
+
+function isEven(number) {
+    return number % 2 === 0;
+}
+const result = isEven(number)
+console.log(result);
