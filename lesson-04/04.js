b28ff9d41027034bf208b737d0e50d734ca67875
Alena Alioshyna <elenakovalski1995@yandex.ru>
2025-06-11 19:35:25 +0300

    three commit

--- lesson-04/04.js
+++ lesson-04/04.js
@@ -15,7 +15,7 @@
 let str = 'hello'
 
 function doubleEachCharacter(string) {
-    let newString = ' ';
+    let newString = '';
     for (let i = 0; i < string.length; i++) {
         newString = newString + string[i] + string[i]
     }
